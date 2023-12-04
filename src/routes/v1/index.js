9f6f993b230cbd2b31d145bd67e96106d4abe9f0
CP-Rahul <rahulcp1401@gmail.com>
2023-12-04 21:13:58 +0530

    Added post city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const airplaneRoute = require('./airplane-routes');
+const cityRoute = require('./city-routes');
 
 const router = express.Router();
 
 router.use('/airplanes', airplaneRoute);
+router.use('/cities', cityRoute);
 
 module.exports = router;
